061a266e57b460842d025297bf1330d6b386e617
MeONaJa <39074420+ogamertv12@users.noreply.github.com>
2023-04-04 12:55:35 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -46,10 +46,12 @@ local function MoveTo(self)
 		self._humanoid:Move((self._waypoints[self._currentWaypoint].Position - self._agent.HumanoidRootPart.Position).Unit, false)
 		RunService.RenderStepped:Wait()
 	end
-	if (self._agent.HumanoidRootPart.Velocity).Magnitude < 0.07 then
-		print("Teleport")
-	elseif (self._agent.HumanoidRootPart.Velocity).Magnitude > 1 and (self._agent.HumanoidRootPart.Velocity).Magnitude < 10 then
-		print("Teleport [2]")
+	if self._currentWaypoint + 1 <= #self._waypoints then
+		if (self._agent.HumanoidRootPart.Velocity).Magnitude < 0.07 then
+			print("[1] Stuck Teleport next")
+		elseif (self._agent.HumanoidRootPart.Velocity).Magnitude > 1 and (self._agent.HumanoidRootPart.Velocity).Magnitude < 10 then
+			print("[2] Stuck Teleport next")
+		end
 	end
 	moveToFinished(self, true)
 end
@@ -75,7 +77,7 @@ end
 function moveToFinished(self, reached)
 	if not getmetatable(self) then return end
 
-	if reached and self._currentWaypoint + 1 <= #self._waypoints  then
+	if reached and self._currentWaypoint + 1 <= #self._waypoints then
 		self._currentWaypoint += 1
 		MoveTo(self)
 	end
